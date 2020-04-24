e660845f6f684c26e44be2a1743c968a17550c6c
Morten Lenschow Hansen <au616040@uni.au.dk>
2020-04-24 20:56:52 +0200

    Finished unit test for SendPageViewModel, started unit test for RefPointsViewModel

---
+++ FCC.Test/ViewModelsTest/TestRefPointsVM.cs
@@ -0,0 +1,20 @@
+﻿using FFC.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCC.Test.ViewModelsTest
+{
+    [TestFixture]
+    class TestRefPointsVM
+    {
+        private RefPointsViewModel _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new RefPointsViewModel();
+        }
+    }
+}
